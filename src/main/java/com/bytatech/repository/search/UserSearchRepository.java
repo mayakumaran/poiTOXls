9b218ba9d1efd427d868d77fccc813c6ad7fb23e
mayabytatech <maya.k.k@lxisoft.com>
2019-10-19 12:38:36 +0530

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/bytatech/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.bytatech.repository.search;
+
+import com.bytatech.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
