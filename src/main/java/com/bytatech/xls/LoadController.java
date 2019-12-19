f92d176dde0b88635c25f17f7b4ed051458279c6
mayabytatech <maya.k.k@lxisoft.com>
2019-12-19 20:07:48 +0530

    AYOOS-57,multipart to object

--- src/main/java/com/bytatech/xls/LoadController.java
+++ src/main/java/com/bytatech/xls/LoadController.java
@@ -15,7 +15,9 @@
  */
 package com.bytatech.xls;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +27,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.bytatech.domain.Drivo;
 import com.bytatech.service.dto.DrivoData;
+import com.bytatech.service.dto.DrivoFile;
 import com.bytatech.service.mapper.DrivoMapper;
 import com.bytatech.web.rest.DrivoResource;
 
@@ -70,6 +74,36 @@ public class LoadController {
 			System.out.println(tempDrive);
 
 			drivoResource.createDrivo(drivoMapper.toDto(tempDrive));
+			
+			tempDriveList.add(tempDrive);
+
+		}
+	}
+	
+	
+	@PostMapping("/byte-import")
+	public void ExcelDatatoDB(@RequestBody DrivoFile file) throws IOException, URISyntaxException {
+
+		List<Drivo> tempDriveList = new ArrayList<Drivo>();
+		
+		 InputStream targetStream = new ByteArrayInputStream(file.getFile());
+		 
+		XSSFWorkbook workbook = new XSSFWorkbook(targetStream);
+		
+		XSSFSheet worksheet = workbook.getSheetAt(0);
+
+		for (int i = 0; i < worksheet.getPhysicalNumberOfRows(); i++) {
+			
+			Drivo tempDrive = new Drivo();
+			XSSFRow row = worksheet.getRow(i);
+			tempDrive.setRegNo(row.getCell(0).getStringCellValue());
+			tempDrive.setOwnerName(row.getCell(1).getStringCellValue());
+			tempDrive.setMobileNo(""+(long)row.getCell(2).getNumericCellValue());
+			tempDrive.setVehdecscr(row.getCell(3).getStringCellValue());
+
+			System.out.println(tempDrive);
+
+			//drivoResource.createDrivo(drivoMapper.toDto(tempDrive));
 			
 			tempDriveList.add(tempDrive);
 
--- target/generated-sources/annotations/com/bytatech/service/mapper/DrivoMapperImpl.java
+++ target/generated-sources/annotations/com/bytatech/service/mapper/DrivoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-12-19T17:01:16+0530",
+    date = "2019-12-19T19:36:03+0530",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_222 (Private Build)"
 )
 @Component
